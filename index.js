2b63869e11dc2ddf8baa812c0ebc9e64cbea6a06
Artur <info@aigroupusa.com>
2021-12-15 19:16:57 -0500

    commit tree 6c46fb9a5f47280c93710abf7d2428fc432df881

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+
+
+function submitData (name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          addIdToDOM(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+}
+
+function addIdToDOM(obj) {
+    const body = document.querySelector("body")
+    const h2 = document.createElement('h2');
+    h2.innerHTML = "id: " + obj.id;
+    body.appendChild(h2);
+}
+
+
+// submitData ("John", "test@email.com")
+
